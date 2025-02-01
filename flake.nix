fe86cb95f3bb10bd95664014654f2882523ca5a3
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-02-01 17:35:58 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      urL = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, disko, ... }@inputs: let
+    systems = [
+      { name = "home"; hostname = "nix"; }
+    ];
+  in {
+    nixosConfigurations = builtins.listToAttrs(map (device: {
+      name = device.name;
+      value = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          meta = {
+            hostname = device.hostname;
+          };
+        };
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./config.nix
+          ./hardeware-config.nix
+          ./disko-config.nix
+        ];
+      };
+    }) systems);
+  };
+}
