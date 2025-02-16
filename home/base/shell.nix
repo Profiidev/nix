5dff4baafa0a036a912f2af28ae2e72a0bc41fa9
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-02-16 22:16:34 +0100

    feat: added fish

--- home/base/shell.nix
+++ home/base/shell.nix
@@ -9,4 +9,23 @@
       exec fish
     '';
   };
+
+  programs.fish = {
+    enable = true;
+    generateCompletions = true;
+
+    shellInit = ''
+      set fish_greeting
+      export WEBKIT_DISABLE_COMPOSITING_MODE=1
+      export RUSTC_WRAPPER=/home/profidev/.cargo/bin/sccache
+      export SCCACHE_CACHE_SIZE="50G"
+      export PATH="$HOME/.cargo/bin:$PATH"
+      export PATH="$HOME/go/bin:$PATH"
+    '';
+    shellInitLast = ''
+      fastfetch
+    '';
+
+    shellAliases = { k = "kubectl"; };
+  };
 }
