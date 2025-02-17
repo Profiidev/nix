ac20bfb22459d2f635ece8e631f98773e58ec3de
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-02-17 22:44:13 +0100

    fix: entries not path

--- home/base/wallpaper.nix
+++ home/base/wallpaper.nix
@@ -1,26 +1,31 @@
 { pkgs, ... }:
 
-{
+let
+  script = pkgs.writeScriptBin "wallpaper.sh" ''
+    #!/bin/bash
+    wget https://profidev.io/backend/services/apod/random -O wallpaper.png
+
+    pwd=$(pwd)
+
+    gsettings set org.gnome.desktop.background picture-uri-dark $pwd/wallpaper.png
+  '';
+
+  desktop = pkgs.writeTextFile {
+    name = "wallpaper.desktop";
+    test = ''
+      [Desktop Entry]
+        Type=Application
+        Name=Wallpaper
+        Exec=${script}/bin/wallpaper.sh
+        Terminal=false
+        NoDisplay=true
+        X-GNOME-Autostart-enabled=true
+    '';
+  };
+in {
   xdg.autostart = {
     enable = true;
-    entries = [ "wallpaper.desktop" ];
-  };
 
-  xdg.desktopEntries = {
-    wallpaper = {
-      name = "Wallpaper";
-      exec = "${
-          toString (pkgs.writeScriptBin "wallpaper.sh" ''
-            #!/bin/bash
-            wget https://profidev.io/backend/services/apod/random -O wallpaper.png
-
-            pwd=$(pwd)
-
-            gsettings set org.gnome.desktop.background picture-uri-dark $pwd/wallpaper.png
-          '')
-        }/bin/wallpaper.sh";
-      terminal = false;
-      noDisplay = true;
-    };
+    entries = [ desktop ];
   };
 }
