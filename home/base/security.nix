048891810049c31c076dcf24c093b15af91350b0
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-02-18 17:40:11 +0100

    fix: moved keyring unlocker to home

--- home/base/security.nix
+++ home/base/security.nix
@@ -1,11 +1,40 @@
 { pkgs, lib, userSpec, config, ... }:
 
+let
+  keyring-unlocker = pkgs.stdenv.mkDerivation {
+    name = "keyring-unlocker";
+    src = (pkgs.fetchFromGitHub {
+      owner = "recolic";
+      repo = "gnome-keyring-yubikey-unlock";
+      rev = "bc874e72e6f13b7385049fa4ea597dd8be6c72be";
+      fetchSubmodules = true;
+      hash = "sha256-SnmzUy+l4WtwYcpmZyWBeGjGJhtR91PQB3nDaOlaOkM=";
+    });
+    buildInputs = [ pkgs.git ];
+
+    buildPhase = ''
+      sed -i 's|bin/unlock_keyrings|unlock_keyrings|' unlock_keyrings.sh
+      sed -i 's|/bin/bash|/usr/bin/env bash|' unlock_keyrings.sh
+      cd src
+      sed -i 's|#include <iostream>|#include <iostream>\n#include <cstdint>|' lib/rlib/stdio.hpp
+      make KEYRING_IMPL=standalone
+      cd ..
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp bin/unlock_keyrings $out/bin
+      cp unlock_keyrings.sh $out/bin
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     yubikey-manager
     yubikey-manager-qt
     yubikey-personalization
     yubikey-personalization-gui
+
+    keyring-unlocker
   ];
 
   home.file.".config/Yubico/u2f_keys".text = userSpec.login_key;
@@ -22,7 +51,7 @@
 
     Service = {
       Type = "oneshot";
-      ExecStart = "${pkgs.keyring-unlocker}/bin/unlock-keyring.sh ${
+      ExecStart = "${keyring-unlocker}/bin/unlock-keyring.sh ${
           config.sops.secrets."keyring_keys/${userSpec.username}".path
         } ${config.sops.secrets."yubikey_pins/${userSpec.username}"}";
     };
--- system/services/security.nix
+++ system/services/security.nix
@@ -1,34 +1,6 @@
 { pkgs, ... }:
 
-let
-  keyring-unlocker = pkgs.stdenv.mkDerivation {
-    name = "keyring-unlocker";
-    src = (pkgs.fetchFromGitHub {
-      owner = "recolic";
-      repo = "gnome-keyring-yubikey-unlock";
-      rev = "bc874e72e6f13b7385049fa4ea597dd8be6c72be";
-      fetchSubmodules = true;
-      hash = "sha256-SnmzUy+l4WtwYcpmZyWBeGjGJhtR91PQB3nDaOlaOkM=";
-    });
-    buildInputs = [ pkgs.git ];
-
-    buildPhase = ''
-      sed -i 's|bin/unlock_keyrings|unlock_keyrings|' unlock_keyrings.sh
-      sed -i 's|/bin/bash|/usr/bin/env bash|' unlock_keyrings.sh
-      cd src
-      sed -i 's|#include <iostream>|#include <iostream>\n#include <cstdint>|' lib/rlib/stdio.hpp
-      make KEYRING_IMPL=standalone
-      cd ..
-    '';
-    installPhase = ''
-      mkdir -p $out/bin
-      cp bin/unlock_keyrings $out/bin
-      cp unlock_keyrings.sh $out/bin
-    '';
-  };
-in {
-  environment.systemPackages = [ keyring-unlocker ];
-
+{
   security.pam.services = {
     login.u2fAuth = true;
     gdm-password.u2fAuth = true;
