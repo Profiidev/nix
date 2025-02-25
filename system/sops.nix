6501a0011f854a8b2ebeff3dd0670df357d0e79b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-02-25 21:12:34 +0100

    fix: age key not saved separatly

--- system/sops.nix
+++ system/sops.nix
@@ -13,11 +13,12 @@ in {
     acc // {
       # These age keys are are unique for the user on each host and are generated on their own (i.e. they are not derived
       # from an ssh key).
-      "keys/age" = {
+      "keys/age_${spec.username}" = {
         owner = config.users.users.${spec.username}.name;
         inherit (config.users.users.${spec.username}) group;
         # We need to ensure the entire directory structure is that of the user...
         path = "${spec.home}/.config/sops/age/keys.txt";
+        key = "keys/age";
       };
       # extract password/username to /run/secrets-for-users/ so it can be used to create the user
       "passwords/${spec.username}" = {
