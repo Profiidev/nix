5e175147d9e290b42273e0578ed4e35ebbbd00d0
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-02 14:45:31 +0100

    feat: added laptop disk id

--- hosts/profiles/laptop/config.nix
+++ hosts/profiles/laptop/config.nix
@@ -5,7 +5,7 @@
     (../../disks/btrfs-luks.nix)
     {
       _module.args = {
-        disk = "/dev/disk/by-id/<id>";
+        disk = "/dev/disk/by-id/nvme-PCIe-8_SSD_1TB_YMA11T0JA2133004AD";
         withSwap = true;
         swapSize = 16;
       };
