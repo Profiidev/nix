bcaad7c2bd39167a7bb4d955c96ff2e8bdc47c08
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-03 23:05:24 +0100

    feat: removed logout when removing yubikey

--- system/services/security.nix
+++ system/services/security.nix
@@ -14,7 +14,7 @@
   };
 
   services.pcscd.enable = true;
-
+/*
   services.udev.extraRules = ''
     ACTION=="remove",\
      ENV{ID_BUS}=="usb",\
@@ -22,5 +22,5 @@
      ENV{ID_VENDOR_ID}=="1050",\
      ENV{ID_VENDOR}=="Yubico",\
      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-  '';
+  '';*/
 }
