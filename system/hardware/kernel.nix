a59af411b8553dd6bf8af26fd6e45bf108a19387
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-05-04 23:47:45 +0200

    fix: silent boot

--- system/hardware/kernel.nix
+++ system/hardware/kernel.nix
@@ -2,7 +2,6 @@
 
 {
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.consoleLogLevel = 0;
 
   boot.initrd = {
     systemd.enable = true;
@@ -14,13 +13,15 @@
     lidSwitchExternalPower = "hibernate";
   };
 
-  consoleLogLevel = 3;
-  initrd.verbose = false;
-  kernelParams = [
-    "quiet"
-    "splash"
-    "boot.shell_on_fail"
-    "udev.log_priority=3"
-    "rd.systemd.show_status=auto"
-  ];
+  boot = {
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
 }
