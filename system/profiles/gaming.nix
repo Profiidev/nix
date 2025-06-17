6945b571592f66b59644674341b5ab745b016f11
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-06-17 15:24:41 +0200

    feat: added sunshine

--- system/profiles/gaming.nix
+++ system/profiles/gaming.nix
@@ -1,5 +1,10 @@
 { ... }:
 
 {
-  imports = [ ../apps/gamemode.nix ../apps/games.nix ../hardware/nvidia.nix ];
+  imports = [
+    ../apps/gamemode.nix
+    ../apps/games.nix
+    ../hardware/nvidia.nix
+    ../services/sunshine.nix
+  ];
 }
