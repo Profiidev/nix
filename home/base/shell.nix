3cab9d50550106622f85e97cdc44d15fc3e86fe7
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-07-14 14:12:13 +0200

    fix: fastfetch shell init

--- home/base/shell.nix
+++ home/base/shell.nix
@@ -28,7 +28,8 @@ in
       export PATH="$HOME/.cargo/bin:$PATH"
       export PATH="$HOME/go/bin:$PATH"
     '';
-    shellInitLast = ''
+    
+    interactiveShellInit = ''
       fastfetch
     '';
 
