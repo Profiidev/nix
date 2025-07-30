517e4f615033d8cc3a5c509b4dd61aae458381af
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-07-30 13:59:54 +0200

    feat: added proton to laptop

--- system/apps/laptop.nix
+++ system/apps/laptop.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, inputs, ... }:
 
 {
   services.cloudflare-warp = {
     enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    inputs.proton.packages.${system}.default
+  ];
 }
