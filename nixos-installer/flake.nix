c76954fa663d5040559b3ffdf6404c208c328023
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-18 22:43:35 +0200

    refactor: better installer flake

--- nixos-installer/flake.nix
+++ nixos-installer/flake.nix
@@ -3,51 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    disko.url =
-      "github:nix-community/disko"; # Declarative partitioning and formatting
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       inherit (self) outputs;
 
       minimalSpecialArgs = {
         inherit inputs outputs;
-        lib = nixpkgs.lib.extend
-          (self: super: { custom = import ../lib { inherit (nixpkgs) lib; }; });
+        lib = nixpkgs.lib.extend (self: super: { custom = import ../lib { inherit (nixpkgs) lib; }; });
       };
 
-      newConfig = name: disk: swapSize: useLuks:
-        (let
-          diskSpecPath = if useLuks then
-            ../hosts/disks/btrfs-luks.nix
-          else
-            ../hosts/disks/btrfs.nix;
-        in nixpkgs.lib.nixosSystem {
+      newConfig =
+        name:
+        (nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = minimalSpecialArgs;
           modules = [
-            inputs.disko.nixosModules.disko
-            diskSpecPath
-            {
-              _module.args = {
-                inherit disk;
-                withSwap = swapSize > 0;
-                swapSize = builtins.toString swapSize;
-              };
-            }
             ./minimal-configuration.nix
             ../hosts/profiles/${name}/hardware-config.nix
 
             { networking.hostName = name; }
           ];
         });
-    in {
+    in
+    {
       nixosConfigurations = {
         # host = newConfig "name" disk" "swapSize" "useLuks"
         # Swap size is in GiB
-        home = newConfig "home" "/dev/disk/by-id/nvme-eui.0025384b3140a47d" 32 false;
-        laptop = newConfig "laptop" "/dev/disk/by-id/nvme-PCIe-8_SSD_1TB_YMA11T0JA2133004AD" 16 true;
+        home = newConfig "home";
+        laptop = newConfig "laptop";
       };
     };
 }
