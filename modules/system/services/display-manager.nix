2ce2fbe3df58eae6ea9717e7f242c3fe35fcd47f
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-29 16:48:57 +0200

    feat: switch to cosmic de

--- modules/system/services/display-manager.nix
+++ modules/system/services/display-manager.nix
@@ -4,12 +4,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  #services.displayManager.gdm.enable = true;
+  #services.desktopManager.gnome.enable = true;
 
-  #services.displayManager.cosmic-greeter.enable = true;
-  #services.desktopManager.cosmic.enable = true;
-  #services.desktopManager.cosmic.xwayland.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.xwayland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
