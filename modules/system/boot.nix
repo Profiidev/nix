a216146097a21eee2a65b173c1941e8c6693ec99
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-30 17:08:39 +0200

    feat: hide boot menu

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -10,6 +10,7 @@
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
+  boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
