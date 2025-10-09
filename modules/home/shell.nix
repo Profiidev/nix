97e79b72b5eb41b0dd05b46729b5c20e333b94c6
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-09 09:57:38 +0200

    feat: added envsource shell function

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 
-let
-in
 {
   programs.bash = {
     enable = true;
@@ -81,6 +79,25 @@ in
       dco = "docker compose up";
       clr = "clear";
       k9s = "k9s -c ctx";
+    };
+
+    functions = {
+      envsource = {
+        body = ''
+          set -f envfile "$argv"
+          if not test -f "$envfile"
+            echo "Unable to load $envfile"
+            return 1
+          end
+          while read line
+            if not string match -qr '^#|^$' "$line"
+              set item (string split -m 1 '=' $line)
+              set -gx $item[1] $item[2]
+              echo "Exported key $item[1]"
+            end
+          end < "$envfile"
+        '';
+      };
     };
 
     plugins = [
