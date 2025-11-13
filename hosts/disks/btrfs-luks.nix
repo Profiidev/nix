d97f1e59a56937e00c4261088b6ec170472546ee
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-11-13 19:45:21 +0100

    fix: reduced fido2 token delay

--- hosts/disks/btrfs-luks.nix
+++ hosts/disks/btrfs-luks.nix
@@ -31,7 +31,7 @@
                   allowDiscards = true;
                   # https://github.com/hmajid2301/dotfiles/blob/a0b511c79b11d9b4afe2a5e2b7eedb2af23e288f/systems/x86_64-linux/framework/disks.nix#L36
                   crypttabExtraOpts =
-                    [ "fido2-device=auto" "token-timeout=10" ];
+                    [ "fido2-device=auto" "token-timeout=5" ];
                 };
                 # Subvolumes must set a mountpoint in order to be mounted,
                 # unless their parent is mounted
