c6fa49820761bf21442cf96b0213a4659147dd58
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-12-15 10:47:52 +0100

    feat: allow remote reboot

--- modules/system/services/security.nix
+++ modules/system/services/security.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   security.pam.services = {
@@ -14,13 +14,39 @@
   };
 
   services.pcscd.enable = true;
-/*
-  services.udev.extraRules = ''
-    ACTION=="remove",\
-     ENV{ID_BUS}=="usb",\
-     ENV{ID_MODEL_ID}=="0407",\
-     ENV{ID_VENDOR_ID}=="1050",\
-     ENV{ID_VENDOR}=="Yubico",\
-     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-  '';*/
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function (action, subject) {
+      if (
+        (action.id == "org.freedesktop.login1.reboot" ||
+         action.id == "org.freedesktop.login1.power-off" ||
+         action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+         action.id == "org.freedesktop.login1.power-off-multiple-sessions") &&
+        (subject.user == "ha_power")
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+  users.users.ha_power = {
+    isNormalUser = true;
+    createHome = lib.mkForce false;
+    group = "ha_power";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFTMajOlMBaHKWMnYsSyHRO0gC2BczCsIhlzwH3EFdP root@a0d7b954-ssh"
+    ];
+  };
+
+  users.groups.ha_power = { };
+  /*
+    services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    '';
+  */
 }
