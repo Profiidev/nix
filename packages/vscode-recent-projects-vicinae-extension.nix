22da8ab810fb59ecb072334fb0532eb344322e3a
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-12-24 00:53:09 +0100

    fix: huge src download

--- packages/vscode-recent-projects-vicinae-extension.nix
+++ packages/vscode-recent-projects-vicinae-extension.nix
@@ -14,6 +14,10 @@ mkVicinaeExtension {
       repo = "extensions";
       rev = "3745b33cbc5ae69d99eb0ded423eab1b494272a0";
       hash = "sha256-nWG2KumFe8X8c898F95fIZKxGerp6PRlTaoL+AtSZAQ=";
+
+      sparseCheckout = [
+        "extensions/visual-studio-code-recent-projects"
+      ];
     }
   }/extensions/visual-studio-code-recent-projects";
 
