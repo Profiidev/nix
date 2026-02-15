d283d0091c663dbc1ff864fc8d215af476b75b21
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-02-15 12:54:24 +0100

    fix: fprint to high timeout

--- modules/system/services/security.nix
+++ modules/system/services/security.nix
@@ -4,7 +4,7 @@
   security.pam.services = {
     login.u2fAuth = true;
     login.fprintAuth = true;
-    login.rules.auth.fprintd.settings.timeout = 100000;
+    login.rules.auth.fprintd.settings.timeout = 99;
     gdm-password.u2fAuth = true;
     gdm-password.enableGnomeKeyring = true;
     sddm.u2fAuth = true;
