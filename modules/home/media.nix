78512b59be7962b7922adadcf291801a76996c6d
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-06 19:49:12 +0200

    feat: added udiskie

---
+++ modules/home/media.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  services.udiskie = {
+    enable = true;
+    settings = {
+      program_options = {
+        file_manager = "${pkgs.nemo-with-extensions}/bin/nemo";
+      };
+    };
+  };
+}
--- modules/system/services/media.nix
+++ modules/system/services/media.nix
@@ -5,4 +5,6 @@
     enable = true;
     mountOnMedia = true;
   };
+
+  services.gvfs.enable = true;
 }
