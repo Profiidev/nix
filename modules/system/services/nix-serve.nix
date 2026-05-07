ceeeb8d3f825e1b374e8f711d35ffa8b81bbf8bb
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-07 08:40:49 +0200

    fix: set priority of local cache

--- modules/system/services/nix-serve.nix
+++ modules/system/services/nix-serve.nix
@@ -1,8 +1,14 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.nix-serve = {
     enable = true;
+
+    package = pkgs.nix-serve-ng;
+    extraParams = lib.concatStringsSep " " [
+      "--priority 100"
+    ];
+
     secretKeyFile = config.sops.secrets."store_key/private".path;
   };
 }
