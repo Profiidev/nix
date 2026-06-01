39641dbc7e1b0425e7b82e967322bcb3bf768a7f
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-01 14:18:35 +0200

    feat: added yazi

---
+++ modules/home/yazi.nix
@@ -0,0 +1,28 @@
+{ ... }:
+
+{
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = true;
+    shellWrapperName = "y";
+
+    settings = {
+      mgr = {
+        sort_by = "natural";
+        sort_sensitive = false;
+        sort_dir_first = true;
+        show_hidden = true;
+        show_symlink = true;
+      };
+      preview = {
+        max_width = 1000;
+        max_height = 1000;
+      };
+    };
+
+    initLua = ''
+      -- require("full-border"):setup()
+      -- require("starship"):setup()
+    '';
+  };
+}
